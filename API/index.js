bb22590b7b203fb0b453b529260f64da66b946a7
Antônio José Martins Mascarenhas <amartinsmascarenhas@gmail.com>
2023-05-25 17:44:10 -0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,26 +1,27 @@
-import express from 'express'
-import {retornaFato, validaAno} from './AnoFato.js'
-import cors from 'cors'
+import express from 'express';
+import { retornaFato, validaAno } from './AnoFato.js';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
 app.use(cors({
-    origin: 'https://consumindo-api-criada.vercel.app/',
-  }))
+  origin: 'http://127.0.0.1:5500',
+}));
 
-app.get("/",(req, res) => {
-    let ano = req.query.ano
-    
-    if(validaAno(ano)){
-        let fato = retornaFato(ano)
-        
-        res.json({fato: fato})
-    }else{
-        res.status(404).json({error: "Ano ínvalido ou digitação errada"})
-    }
-})
+app.get("/", (req, res) => {
+  let ano = req.query.ano;
+
+  if (validaAno(ano)) {
+    let fato = retornaFato(ano);
+
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.json({ fato: fato });
+  } else {
+    res.status(404).json({ error: "Ano inválido ou digitação errada" });
+  }
+});
 
 app.listen(8080, () => {
-    let data = new Date()
-    console.log(`Servidor iniciado em ${data}`)
-})
+  let data = new Date();
+  console.log(`Servidor iniciado em ${data}`);
+});
